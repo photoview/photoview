900f66475b35d80df5e7b373787c0c32acb7454b
johnymontana <lyonwj@gmail.com>
2018-06-27 17:00:01 -0600

    add Material-UI and data Table

--- ui/src/UserList.js
+++ ui/src/UserList.js
@@ -1,34 +1,161 @@
 import React from "react";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
-import './UserList.css';
+import "./UserList.css";
+import { withStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Tooltip from "@material-ui/core/Tooltip";
+import Paper from "@material-ui/core/Paper";
+import { TableSortLabel } from "@material-ui/core";
 
-const UserList = () => (
-  <Query
-    query={gql`
-      {
-        users(first: 10) {
-          name
-        }
-      }
-    `}
-  >
-    {({ loading, error, data }) => {
-      if (loading) return <p>Loading...</p>;
-      if (error) return <p>Error</p>;
+const styles = theme => ({
+  root: {
+    maxWidth: 700,
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto",
+    margin: "auto"
+  },
+  table: {
+    minWidth: 700
+  }
+});
 
-      return (
-          <div className="UserList">
-          <h1>Users:</h1>
-        <ul>
-          {data.users.map(({name}, i) => (
-          <li key={i}>{name}</li>
-          ))}
-        </ul>
-        </div>
-      );
-    }}
-  </Query>
-);
+function getSorting(order, orderBy) {
+  return order === "desc"
+    ? (a, b) => (b[orderBy] < a[orderBy] ? -1 : 1)
+    : (a, b) => (a[orderBy] < b[orderBy] ? -1 : 1);
+}
 
-export default UserList;
+class UserList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      order: "asc",
+      orderBy: "avgStars"
+    };
+  }
+
+  handleSortRequest = property => {
+    const orderBy = property;
+    let order = "desc";
+
+    if (this.state.orderBy === property && this.state.order === "desc") {
+      order = "asc";
+    }
+
+    this.setState({ order, orderBy });
+  };
+
+  render() {
+    const { order, orderBy } = this.state;
+    return (
+      <Query
+        query={gql`
+          {
+            users(first: 10, offset: 0) {
+              id
+              name
+              avgStars
+              numReviews
+            }
+          }
+        `}
+      >
+        {({ loading, error, data }) => {
+          if (loading) return <p>Loading...</p>;
+          if (error) return <p>Error</p>;
+
+          return (
+            <Paper className={this.props.classes.root}>
+              <Table className={this.props.classes.table}>
+                <TableHead>
+                  <TableRow>
+                    <TableCell
+                      key="name"
+                      sortDirection={orderBy === "name" ? order : false}
+                    >
+                      <Tooltip
+                        title="Sort"
+                        placement="bottom-start"
+                        enterDelay={300}
+                      >
+                        <TableSortLabel
+                          active={orderBy === "name"}
+                          direction={order}
+                          onClick={() => this.handleSortRequest("name")}
+                        >
+                          Name
+                        </TableSortLabel>
+                      </Tooltip>
+                    </TableCell>
+                    <TableCell
+                      key="avgStars"
+                      sortDirection={orderBy === "avgStars" ? order : false}
+                      numeric
+                    >
+                      <Tooltip
+                        title="Sort"
+                        placement="bottom-end"
+                        enterDelay={300}
+                      >
+                        <TableSortLabel
+                          active={orderBy === "avgStars"}
+                          direction={order}
+                          onClick={() => this.handleSortRequest("avgStars")}
+                        >
+                          Average Stars
+                        </TableSortLabel>
+                      </Tooltip>
+                    </TableCell>
+                    <TableCell
+                      key="numReviews"
+                      sortDirection={orderBy === "numReviews" ? order : false}
+                      numeric
+                    >
+                      <Tooltip
+                        title="Sort"
+                        placement="bottom-start"
+                        enterDelay={300}
+                      >
+                        <TableSortLabel
+                          active={orderBy === "numReviews"}
+                          direction={order}
+                          onClick={() => this.handleSortRequest("numReviews")}
+                        >
+                          Number of Reviews
+                        </TableSortLabel>
+                      </Tooltip>
+                    </TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {data.users
+                    .slice()
+                    .sort(getSorting(order, orderBy))
+                    .map(n => {
+                      return (
+                        <TableRow key={n.id}>
+                          <TableCell component="th" scope="row">
+                            {n.name}
+                          </TableCell>
+                          <TableCell numeric>{n.avgStars}</TableCell>
+                          <TableCell numeric>{n.numReviews}</TableCell>
+                        </TableRow>
+                      );
+                    })}
+                </TableBody>
+              </Table>
+            </Paper>
+          );
+        }}
+      </Query>
+    );
+  }
+}
+
+export default withStyles(styles)(UserList);
