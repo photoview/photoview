fd46a855a7ee41f8ab5f2a142e8aaf358fb7810b
viktorstrate <viktorstrate@gmail.com>
2019-07-18 17:40:54 +0200

    Working on animations

--- ui/src/Pages/HomePage.js
+++ ui/src/Pages/HomePage.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import Albums from '../Albums'
 
 class HomePage extends Component {
   render() {
     return (
       <div>
         <h1>Home</h1>
+        <Albums />
       </div>
     )
   }
--- ui/src/Pages/LoginPage.js
+++ ui/src/Pages/LoginPage.js
@@ -14,6 +14,13 @@ const authorizeMutation = gql`
   }
 `
 
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date()
+  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000)
+  var expires = 'expires=' + d.toUTCString()
+  document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/'
+}
+
 class LoginPage extends Component {
   constructor(props) {
     super(props)
@@ -57,6 +64,7 @@ class LoginPage extends Component {
 
               if (success) {
                 localStorage.setItem('token', token)
+                setCookie('token', token, 360)
                 window.location = '/'
               }
             }}
