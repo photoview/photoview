56800b14a80995fb79cf764309504ef8566134ec
viktorstrate <viktorstrate@gmail.com>
2019-07-20 18:25:15 +0200

    Sync message popup

---
+++ ui/src/Messages.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import { Message, Progress } from 'semantic-ui-react'
+import gql from 'graphql-tag'
+import { Subscription } from 'react-apollo'
+
+const syncSubscription = gql`
+  subscription syncSubscription {
+    scannerStatusUpdate {
+      finished
+      error
+      errorMessage
+      progress
+    }
+  }
+`
+
+const Container = styled.div`
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  width: 500px;
+`
+
+const MessageProgress = ({ header, content, percent, ...props }) => {
+  const StyledProgress = styled(Progress)`
+    position: absolute !important;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+  `
+
+  return (
+    <Message floating {...props}>
+      <Message.Content>
+        <Message.Header>{header}</Message.Header>
+        {content}
+        <StyledProgress
+          percent={percent}
+          size="tiny"
+          attached="bottom"
+          indicating
+        />
+      </Message.Content>
+    </Message>
+  )
+}
+
+class Messages extends Component {
+  render() {
+    return (
+      <Container>
+        <Subscription subscription={syncSubscription} shouldResubscribe>
+          {({ loading, error, data }) => {
+            if (error) return <div>error {error.message}</div>
+            if (loading) return null
+
+            console.log('Data update', data)
+            const update = data.scannerStatusUpdate
+
+            return (
+              <MessageProgress
+                header={update.finished ? 'Synced' : 'Syncing'}
+                content={
+                  update.finished ? 'Finished syncing' : 'Syncing in progress'
+                }
+                percent={update.progress}
+              />
+            )
+          }}
+        </Subscription>
+      </Container>
+    )
+  }
+}
+
+export default Messages
--- ui/src/apolloClient.js
+++ ui/src/apolloClient.js
@@ -1,14 +1,40 @@
 import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { HttpLink } from 'apollo-link-http'
+import { WebSocketLink } from 'apollo-link-ws'
 import { onError } from 'apollo-link-error'
 import { setContext } from 'apollo-link-context'
-import { ApolloLink } from 'apollo-link'
+import { ApolloLink, split } from 'apollo-link'
+import { getMainDefinition } from 'apollo-utilities'
 
 const httpLink = new HttpLink({
   uri: process.env.REACT_APP_GRAPHQL_URI,
   credentials: 'same-origin',
 })
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4001/graphql`,
+  credentials: 'same-origin',
+  options: {
+    reconnect: true,
+    connectionParams: {
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+  },
+})
+
+const link = split(
+  // split based on operation type
+  ({ query }) => {
+    const definition = getMainDefinition(query)
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    )
+  },
+  wsLink,
+  httpLink
+)
 
 const linkError = onError(({ graphQLErrors, networkError }) => {
   if (graphQLErrors)
@@ -17,7 +43,8 @@ const linkError = onError(({ graphQLErrors, networkError }) => {
         `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
       )
     )
-  if (networkError) console.log(`[Network error]: ${networkError}`)
+  if (networkError)
+    console.log(`[Network error]: ${JSON.stringify(networkError)}`)
 })
 
 const authLink = setContext((_, { headers }) => {
@@ -33,7 +60,7 @@ const authLink = setContext((_, { headers }) => {
 })
 
 const client = new ApolloClient({
-  link: ApolloLink.from([linkError, authLink.concat(httpLink)]),
+  link: ApolloLink.from([linkError, authLink.concat(link)]),
   cache: new InMemoryCache(),
 })
 
