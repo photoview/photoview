f57388e27691cffa927f543f16ae294f6341c3e9
viktorstrate <viktorstrate@gmail.com>
2020-02-21 12:13:49 +0100

    Use crypto/rand for improved security

--- api/utils/utils.go
+++ api/utils/utils.go
@@ -1,14 +1,25 @@
 package utils
 
-import "math/rand"
+import "crypto/rand"
+
+import "math/big"
+
+import "log"
 
 func GenerateToken() string {
 	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 	const length = 8
 
+	charLen := big.NewInt(int64(len(charset)))
+
 	b := make([]byte, length)
 	for i := range b {
-		b[i] = charset[rand.Intn(len(charset))]
+
+		n, err := rand.Int(rand.Reader, charLen)
+		if err != nil {
+			log.Fatalf("Could not generate random number: %s\n", err)
+		}
+		b[i] = charset[n.Int64()]
 	}
 	return string(b)
 }
