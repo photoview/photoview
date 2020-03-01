4661bae48d93253a25d3d3eccc40ac7f0348773e
Brandon Davis <git@subdavis.com>
2020-02-29 20:06:18 -0500

    One dockerfile to rule them all

---
+++ Dockerfile
@@ -0,0 +1,34 @@
+# Build UI
+FROM node:10 as ui
+
+ARG GRAPHQL_ENDPOINT
+ENV GRAPHQL_ENDPOINT=${GRAPHQL_ENDPOINT}
+
+RUN mkdir -p /app
+WORKDIR /app
+
+COPY ui/package*.json ./
+RUN npm install
+COPY ui /app
+
+RUN npm run build
+
+# Build API
+FROM golang:alpine AS api
+
+WORKDIR /app
+COPY api /app
+
+RUN go get -d -v ./...
+RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o photoview .
+
+# Copy built app to nginx environment
+FROM alpine:latest
+
+COPY --from=ui /app/dist /ui
+COPY --from=api /app/database/migrations /database/migrations
+COPY --from=api /app/photoview /app/photoview
+
+EXPOSE 80
+
+ENTRYPOINT ["/app/photoview"]
--- api/server.go
+++ api/server.go
@@ -6,6 +6,7 @@ import (
 	"net/url"
 	"os"
 	"path"
+	"path/filepath"
 
 	"github.com/gorilla/mux"
 
@@ -22,6 +23,49 @@ import (
 )
 
 const defaultPort = "4001"
+
+// spaHandler implements the http.Handler interface, so we can use it
+// to respond to HTTP requests. The path to the static directory and
+// path to the index file within that static directory are used to
+// serve the SPA in the given static directory.
+type spaHandler struct {
+	staticPath string
+	indexPath  string
+}
+
+// ServeHTTP inspects the URL path to locate a file within the static dir
+// on the SPA handler. If a file is found, it will be served. If not, the
+// file located at the index path on the SPA handler will be served. This
+// is suitable behavior for serving an SPA (single page application).
+func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// get the absolute path to prevent directory traversal
+	path, err := filepath.Abs(r.URL.Path)
+	if err != nil {
+		// if we failed to get the absolute path respond with a 400 bad request
+		// and stop
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// prepend the path with the path to the static directory
+	path = filepath.Join(h.staticPath, path)
+
+	// check whether a file exists at the given path
+	_, err = os.Stat(path)
+	if os.IsNotExist(err) {
+		// file does not exist, serve index.html
+		http.ServeFile(w, r, filepath.Join(h.staticPath, h.indexPath))
+		return
+	} else if err != nil {
+		// if we got an error (that wasn't that the file doesn't exist) stating the
+		// file, return a 500 internal server error and stop
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// otherwise, use http.FileServer to serve the static dir
+	http.FileServer(http.Dir(h.staticPath)).ServeHTTP(w, r)
+}
 
 func main() {
 
@@ -68,14 +112,14 @@ func main() {
 	endpointRouter := rootRouter.PathPrefix(endpointURL.Path).Subrouter()
 
 	if devMode {
-		endpointRouter.Handle("/", handler.Playground("GraphQL playground", path.Join(endpointURL.Path, "/graphql")))
+		endpointRouter.Handle("/api", handler.Playground("GraphQL playground", path.Join(endpointURL.Path, "/graphql")))
 	} else {
-		endpointRouter.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		endpointRouter.HandleFunc("/api", func(w http.ResponseWriter, req *http.Request) {
 			w.Write([]byte("photoview api endpoint"))
 		})
 	}
 
-	endpointRouter.Handle("/graphql",
+	endpointRouter.Handle("/api/graphql",
 		handler.GraphQL(photoview_graphql.NewExecutableSchema(graphqlConfig),
 			handler.IntrospectionEnabled(devMode),
 			handler.WebsocketUpgrader(server.WebsocketUpgrader(devMode)),
@@ -83,8 +127,11 @@ func main() {
 		),
 	)
 
-	photoRouter := endpointRouter.PathPrefix("/photo").Subrouter()
+	photoRouter := endpointRouter.PathPrefix("/api/photo").Subrouter()
 	routes.RegisterPhotoRoutes(db, photoRouter)
+
+	spa := spaHandler{staticPath: "/ui", indexPath: "index.html"}
+	endpointRouter.PathPrefix("/").Handler(spa)
 
 	if devMode {
 		log.Printf("🚀 Graphql playground ready at %s", endpointURL.String())
