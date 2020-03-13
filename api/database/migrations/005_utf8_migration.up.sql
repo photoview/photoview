d3909f4004e0b3d10de401cda50abfe78e30afb1
viktorstrate <viktorstrate@gmail.com>
2020-03-13 17:47:07 +0100

    Update UTF migration to use 4 bytes unicode

--- api/database/migrations/005_utf8_migration.up.sql
+++ api/database/migrations/005_utf8_migration.up.sql
@@ -1,9 +1,9 @@
 -- Migrate all tables in database to use utf8 for better language support
-ALTER TABLE access_token CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
-ALTER TABLE album CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
-ALTER TABLE photo CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
-ALTER TABLE photo_exif CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
-ALTER TABLE photo_url CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
-ALTER TABLE share_token CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
-ALTER TABLE site_info CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
-ALTER TABLE user CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+ALTER TABLE access_token CONVERT TO CHARACTER SET utf8 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE album CONVERT TO CHARACTER SET utf8 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE photo CONVERT TO CHARACTER SET utf8 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE photo_exif CONVERT TO CHARACTER SET utf8 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE photo_url CONVERT TO CHARACTER SET utf8 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE share_token CONVERT TO CHARACTER SET utf8 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE site_info CONVERT TO CHARACTER SET utf8 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE user CONVERT TO CHARACTER SET utf8 COLLATE utf8mb4_unicode_ci;
