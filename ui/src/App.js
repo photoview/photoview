66b54a97f4768ff61c94ca0a0062633d555c7adb
viktorstrate <viktorstrate@gmail.com>
2020-06-15 17:26:52 +0200

    Add dimmer css and change dimmer opacity
    - This solves a bug where clicking on "Change password"
    would make the popup layout incorrectly

--- ui/src/App.js
+++ ui/src/App.js
@@ -9,12 +9,18 @@ const GlobalStyle = createGlobalStyle`
     height: 100%;
     margin: 0;
   }
+
+  /* Make dimmer lighter */
+  .ui.dimmer {
+    background-color: rgba(0, 0, 0, 0.5);
+  }
 `
 
 import 'semantic-ui-css/components/reset.css'
 import 'semantic-ui-css/components/site.css'
 import 'semantic-ui-css/components/transition.css'
 import 'semantic-ui-css/components/menu.css'
+import 'semantic-ui-css/components/dimmer.css'
 
 class App extends Component {
   render() {
