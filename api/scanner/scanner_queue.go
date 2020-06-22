2f4212eeabc79d4b19d5fd4cabbf485aa8b1e5c4
viktorstrate <viktorstrate@gmail.com>
2020-06-22 18:02:21 +0200

    Start on scanner queue + add some tests

---
+++ api/scanner/scanner_queue.go
@@ -0,0 +1,116 @@
+package scanner
+
+import (
+	"database/sql"
+	"errors"
+	"sync"
+
+	"github.com/viktorstrate/photoview/api/graphql/models"
+)
+
+type ScannerJobScope int
+
+const (
+	JOB_SCAN_USER ScannerJobScope = iota
+	JOB_SCAN_ALBUM
+)
+
+type ScannerJob struct {
+	scope ScannerJobScope
+	// Either models.User, models.Album or nil depending on the value of scope
+	model interface{}
+}
+
+func (job *ScannerJob) modelAsUser() (*models.User, error) {
+	user, ok := job.model.(models.User)
+	if !ok {
+		return nil, errors.New("scanner job not of type User")
+	}
+	return &user, nil
+}
+
+func (job *ScannerJob) modelAsAlbum() (*models.Album, error) {
+	album, ok := job.model.(models.Album)
+	if !ok {
+		return nil, errors.New("scanner job not of type Album")
+	}
+	return &album, nil
+}
+
+type ScannerQueue struct {
+	mutex       sync.Mutex
+	idle_chan   chan bool
+	in_progress []ScannerJob
+	up_next     []ScannerJob
+	db          *sql.DB
+}
+
+var global_scanner_queue ScannerQueue
+
+func InitializeScannerQueue(db *sql.DB) {
+	global_scanner_queue = ScannerQueue{
+		idle_chan:   make(chan bool, 1),
+		in_progress: make([]ScannerJob, 0),
+		up_next:     make([]ScannerJob, 0),
+		db:          db,
+	}
+}
+
+func (job *ScannerJob) Run() {
+	// TODO: Not implemented
+}
+
+func (queue *ScannerQueue) AddJob(job *ScannerJob) error {
+	if exists, err := queue.jobOnQueue(job); exists || err != nil {
+		return err
+	}
+	queue.up_next = append(queue.up_next, *job)
+	queue.Notify()
+
+	return nil
+}
+
+func (queue *ScannerQueue) Notify() bool {
+	select {
+	case queue.idle_chan <- true:
+		return true
+	default:
+		return false
+	}
+}
+
+func (queue *ScannerQueue) jobOnQueue(job *ScannerJob) (bool, error) {
+	queue.mutex.Lock()
+	defer queue.mutex.Unlock()
+
+	scannerJobs := append(queue.in_progress, queue.up_next...)
+
+	for _, scannerJob := range scannerJobs {
+
+		if scannerJob == *job {
+			return true, nil
+		}
+
+		if scannerJob.scope == JOB_SCAN_USER {
+			user, err := scannerJob.modelAsUser()
+			if err != nil {
+				return true, err
+			}
+
+			if job.scope == JOB_SCAN_ALBUM {
+				album, err := job.modelAsAlbum()
+				if err != nil {
+					return true, err
+				}
+
+				if album.OwnerID == user.UserID {
+					return true, nil
+				}
+
+			}
+		}
+
+	}
+
+	return false, nil
+}
