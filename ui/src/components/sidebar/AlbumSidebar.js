d03525d3157392408c3b26595c172b0f277a94a2
stz184 <ivanov.bg@gmail.com>
2020-10-31 23:50:08 +0200

    AlbumSidebar, AuthorizedRoute, LoginPage are now using hooks instead of the deprecated Query component

--- ui/src/components/sidebar/AlbumSidebar.js
+++ ui/src/components/sidebar/AlbumSidebar.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Query } from '@apollo/client/react/components'
-import { gql } from '@apollo/client'
+import { useQuery, gql } from '@apollo/client'
 import SidebarShare from './Sharing'
 
 const albumQuery = gql`
@@ -14,22 +13,20 @@ const albumQuery = gql`
 `
 
 const AlbumSidebar = ({ albumId }) => {
+  const { loading, error, data } = useQuery(albumQuery, {
+    variables: { id: albumId },
+  })
+
+  if (loading) return <div>Loading...</div>
+  if (error) return <div>{error.message}</div>
+
   return (
     <div>
       <p>Album options</p>
-      <Query query={albumQuery} variables={{ id: albumId }}>
-        {({ loading, error, data }) => {
-          if (loading) return <div>Loading...</div>
-          if (error) return <div>{error.message}</div>
-
-          return (
-            <div>
-              <h1>{data.album.title}</h1>
-              <SidebarShare album={data.album} />
-            </div>
-          )
-        }}
-      </Query>
+      <div>
+        <h1>{data.album.title}</h1>
+        <SidebarShare album={data.album} />
+      </div>
     </div>
   )
 }
