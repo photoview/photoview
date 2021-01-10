41b122c461293f6b14164ed01e18b570b1d5f847
viktorstrate <viktorstrate@gmail.com>
2021-01-10 18:59:01 +0100

    Start on sqlite support
    - TODO: Sqlite doesn't support MD5, all calls to this function should be done
      beforehand.

--- api/database/database.go
+++ api/database/database.go
@@ -4,18 +4,18 @@ import (
 	"log"
 	"net/url"
 	"os"
+	"strings"
 
 	"github.com/photoview/photoview/api/graphql/models"
 	"github.com/pkg/errors"
 
 	"gorm.io/driver/mysql"
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
 
-// SetupDatabase connects to the database using environment variables
-func SetupDatabase() (*gorm.DB, error) {
-
+func getMysqlAddress() (*url.URL, error) {
 	address, err := url.Parse(os.Getenv("MYSQL_URL"))
 	if err != nil {
 		return nil, errors.Wrap(err, "Could not parse mysql url")
@@ -30,15 +30,36 @@ func SetupDatabase() (*gorm.DB, error) {
 	queryValues.Add("parseTime", "true")
 
 	address.RawQuery = queryValues.Encode()
+	return address, nil
+}
 
-	log.Printf("Connecting to database: %s", address)
+// SetupDatabase connects to the database using environment variables
+func SetupDatabase() (*gorm.DB, error) {
 
 	config := gorm.Config{}
 
 	// Enable database debug logging
 	config.Logger = logger.Default.LogMode(logger.Info)
 
-	db, err := gorm.Open(mysql.Open(address.String()), &config)
+	var databaseDialect gorm.Dialector
+	switch strings.ToLower(os.Getenv("PHOTOVIEW_DATABASE_DRIVER")) {
+	case "mysql":
+		mysqlAddress, err := getMysqlAddress()
+		if err != nil {
+			return nil, err
+		}
+		log.Printf("Connecting to database: %s", mysqlAddress)
+		databaseDialect = mysql.Open(mysqlAddress.String())
+
+	case "sqlite":
+		path := os.Getenv("PHOTOVIEW_SQLITE_PATH")
+		if path == "" {
+			path = "photoview.db"
+		}
+		databaseDialect = sqlite.Open(path)
+	}
+
+	db, err := gorm.Open(databaseDialect, &config)
 	if err != nil {
 		return nil, errors.Wrap(err, "could not connect to database")
 	}
