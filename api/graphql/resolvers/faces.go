00fceea4dbd8477ad030651a1d279184574ef918
viktorstrate <viktorstrate@gmail.com>
2021-02-19 17:49:41 +0100

    Work on face merge ui

--- api/graphql/resolvers/faces.go
+++ api/graphql/resolvers/faces.go
@@ -2,10 +2,11 @@ package resolvers
 
 import (
 	"context"
-	"errors"
 
 	"github.com/photoview/photoview/api/graphql/auth"
 	"github.com/photoview/photoview/api/graphql/models"
+	"github.com/pkg/errors"
+	"gorm.io/gorm"
 )
 
 func (r *queryResolver) MyFaceGroups(ctx context.Context, paginate *models.Pagination) ([]*models.FaceGroup, error) {
@@ -74,7 +75,72 @@ func (r *mutationResolver) SetFaceGroupLabel(ctx context.Context, faceGroupID in
 		return nil, errors.New("unauthorized")
 	}
 
-	if err := user.FillAlbums(r.Database); err != nil {
+	faceGroup, err := userOwnedFaceGroup(r.Database, user, faceGroupID)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := r.Database.Model(faceGroup).Update("label", label).Error; err != nil {
+		return nil, err
+	}
+
+	return faceGroup, nil
+}
+
+func (r *mutationResolver) CombineFaceGroups(ctx context.Context, destinationFaceGroupID int, sourceFaceGroupID int) (*models.FaceGroup, error) {
+	user := auth.UserFromContext(ctx)
+	if user == nil {
+		return nil, errors.New("unauthorized")
+	}
+
+	destinationFaceGroup, err := userOwnedFaceGroup(r.Database, user, destinationFaceGroupID)
+	if err != nil {
+		return nil, err
+	}
+
+	sourceFaceGroup, err := userOwnedFaceGroup(r.Database, user, sourceFaceGroupID)
+	if err != nil {
+		return nil, err
+	}
+
+	updateError := r.Database.Transaction(func(tx *gorm.DB) error {
+		if err := tx.Model(&models.ImageFace{}).Where("face_group_id = ?", sourceFaceGroup.ID).Update("face_group_id", destinationFaceGroup.ID).Error; err != nil {
+			return err
+		}
+
+		if err := tx.Delete(&sourceFaceGroup).Error; err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	if updateError != nil {
+		return nil, updateError
+	}
+
+	return destinationFaceGroup, nil
+}
+
+func (r *mutationResolver) MoveImageFace(ctx context.Context, imageFaceID int, newFaceGroupID int) (*models.ImageFace, error) {
+	panic("not implemented")
+}
+
+func (r *mutationResolver) RecognizeUnlabeledFaces(ctx context.Context) ([]*models.ImageFace, error) {
+	panic("not implemented")
+}
+
+func userOwnedFaceGroup(db *gorm.DB, user *models.User, faceGroupID int) (*models.FaceGroup, error) {
+	if user.Admin {
+		var faceGroup models.FaceGroup
+		if err := db.Where("id = ?", faceGroupID).Find(&faceGroup).Error; err != nil {
+			return nil, err
+		}
+
+		return &faceGroup, nil
+	}
+
+	if err := user.FillAlbums(db); err != nil {
 		return nil, err
 	}
 
@@ -84,13 +150,13 @@ func (r *mutationResolver) SetFaceGroupLabel(ctx context.Context, faceGroupID in
 	}
 
 	// Verify that user owns at leat one of the images in the face group
-	imageFaceQuery := r.Database.
+	imageFaceQuery := db.
 		Select("image_faces.id").
 		Table("image_faces").
 		Joins("LEFT JOIN media ON media.id = image_faces.media_id").
 		Where("media.album_id IN (?)", userAlbumIDs)
 
-	faceGroupQuery := r.Database.
+	faceGroupQuery := db.
 		Model(&models.FaceGroup{}).
 		Joins("JOIN image_faces ON face_groups.id = image_faces.face_group_id").
 		Where("face_groups.id = ?", faceGroupID).
@@ -98,24 +164,11 @@ func (r *mutationResolver) SetFaceGroupLabel(ctx context.Context, faceGroupID in
 
 	var faceGroup models.FaceGroup
 	if err := faceGroupQuery.Find(&faceGroup).Error; err != nil {
-		return nil, err
-	}
-
-	if err := r.Database.Model(&faceGroup).Update("label", label).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return nil, errors.Wrap(err, "face group does not exist or is not owned by the user")
+		}
 		return nil, err
 	}
 
 	return &faceGroup, nil
-}
-
-func (r *mutationResolver) CombineFaceGroups(ctx context.Context, destinationFaceGroupID int, sourceFaceGroupID int) (*models.FaceGroup, error) {
-	panic("not implemented")
-}
-
-func (r *mutationResolver) MoveImageFace(ctx context.Context, imageFaceID int, newFaceGroupID int) (*models.ImageFace, error) {
-	panic("not implemented")
-}
-
-func (r *mutationResolver) RecognizeUnlabeledFaces(ctx context.Context) ([]*models.ImageFace, error) {
-	panic("not implemented")
 }
---
+++ ui/src/Pages/PeoplePage/SingleFaceGroup/FaceGroupTitle.js
@@ -0,0 +1,141 @@
+import { useMutation } from '@apollo/client'
+import PropTypes from 'prop-types'
+import React, { useState, useEffect, createRef } from 'react'
+import { Dropdown, Input } from 'semantic-ui-react'
+import styled from 'styled-components'
+import { SET_GROUP_LABEL_MUTATION } from '../PeoplePage'
+import MergeFaceGroupsModal from './MergeFaceGroupsModal'
+
+const TitleWrapper = styled.div`
+  min-height: 3.5em;
+`
+
+const TitleLabel = styled.h1`
+  display: inline-block;
+  color: ${({ labeled }) => (labeled ? 'black' : '#888')};
+  margin-right: 12px;
+`
+
+const TitleDropdown = styled(Dropdown)`
+  vertical-align: middle;
+  margin-top: -10px;
+  color: #888;
+
+  &:hover {
+    color: #1e70bf;
+  }
+`
+
+const FaceGroupTitle = ({ faceGroup }) => {
+  const [editLabel, setEditLabel] = useState(false)
+  const [inputValue, setInputValue] = useState(faceGroup?.label ?? '')
+  const inputRef = createRef()
+  const [mergeModalOpen, setMergeModalOpen] = useState(false)
+
+  const [setGroupLabel, { loading: setLabelLoading }] = useMutation(
+    SET_GROUP_LABEL_MUTATION,
+    {
+      variables: {
+        groupID: faceGroup?.id,
+      },
+    }
+  )
+
+  const resetLabel = () => {
+    setInputValue(faceGroup.label ?? '')
+    setEditLabel(false)
+  }
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus()
+    }
+  }, [inputRef])
+
+  useEffect(() => {
+    if (!setLabelLoading) {
+      resetLabel()
+    }
+  }, [setLabelLoading])
+
+  const onKeyUp = e => {
+    if (e.key == 'Escape') {
+      resetLabel()
+      return
+    }
+
+    if (e.key == 'Enter') {
+      setGroupLabel({
+        variables: {
+          label: e.target.value == '' ? null : e.target.value,
+        },
+      })
+      return
+    }
+  }
+
+  let title
+  if (!editLabel) {
+    title = (
+      <TitleWrapper>
+        <TitleLabel labeled={!!faceGroup?.label}>
+          {faceGroup?.label ?? 'Unlabeled person'}
+        </TitleLabel>
+        <TitleDropdown
+          icon={{
+            name: 'settings',
+            size: 'large',
+          }}
+        >
+          <Dropdown.Menu>
+            <Dropdown.Item
+              icon="pencil"
+              text={faceGroup?.label ? 'Change Label' : 'Add Label'}
+              onClick={() => setEditLabel(true)}
+            />
+            <Dropdown.Item
+              icon="object ungroup"
+              text="Merge Face"
+              alt="Merge this group into another"
+              onClick={() => setMergeModalOpen(true)}
+            />
+          </Dropdown.Menu>
+        </TitleDropdown>
+      </TitleWrapper>
+    )
+  } else {
+    title = (
+      <TitleWrapper>
+        <Input
+          loading={setLabelLoading}
+          ref={inputRef}
+          placeholder="Label"
+          icon="arrow right"
+          value={inputValue}
+          onKeyUp={onKeyUp}
+          onChange={e => setInputValue(e.target.value)}
+          onBlur={() => {
+            resetLabel()
+          }}
+        />
+      </TitleWrapper>
+    )
+  }
+
+  return (
+    <>
+      {title}
+      <MergeFaceGroupsModal
+        open={mergeModalOpen}
+        setOpen={setMergeModalOpen}
+        sourceFaceGroup={faceGroup}
+      />
+    </>
+  )
+}
+
+FaceGroupTitle.propTypes = {
+  faceGroup: PropTypes.object,
+}
+
+export default FaceGroupTitle
