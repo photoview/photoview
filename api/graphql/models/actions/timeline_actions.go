e66a9199a1a5602a06d4fa5aff9997f5ae685511
viktorstrate <viktorstrate@gmail.com>
2021-09-27 20:23:10 +0200

    Add timeline resolver tests

---
+++ api/graphql/models/actions/timeline_actions.go
@@ -0,0 +1,36 @@
+package actions
+
+import (
+	"time"
+
+	"github.com/photoview/photoview/api/graphql/models"
+	"gorm.io/gorm"
+)
+
+func MyTimeline(db *gorm.DB, user *models.User, paginate *models.Pagination, onlyFavorites *bool, fromDate *time.Time) ([]*models.Media, error) {
+	query := db.
+		Joins("JOIN albums ON media.album_id = albums.id").
+		Where("albums.id IN (?)", db.Table("user_albums").Select("user_albums.album_id").Where("user_id = ?", user.ID)).
+		Order("YEAR(media.date_shot) DESC").
+		Order("MONTH(media.date_shot) DESC").
+		Order("DAY(media.date_shot) DESC").
+		Order("albums.title ASC").
+		Order("TIME(media.date_shot) DESC")
+
+	if fromDate != nil {
+		query = query.Where("media.date_shot < ?", fromDate)
+	}
+
+	if onlyFavorites != nil && *onlyFavorites == true {
+		query = query.Where("media.id IN (?)", db.Table("user_media_data").Select("user_media_data.media_id").Where("user_media_data.user_id = ?", user.ID).Where("user_media_data.favorite = 1"))
+	}
+
+	query = models.FormatSQL(query, nil, paginate)
+
+	var media []*models.Media
+	if err := query.Find(&media).Error; err != nil {
+		return nil, err
+	}
+
+	return media, nil
+}
--- api/graphql/resolvers/timeline.go
+++ api/graphql/resolvers/timeline.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/photoview/photoview/api/graphql/auth"
 	"github.com/photoview/photoview/api/graphql/models"
+	"github.com/photoview/photoview/api/graphql/models/actions"
 )
 
 func (r *queryResolver) MyTimeline(ctx context.Context, paginate *models.Pagination, onlyFavorites *bool, fromDate *time.Time) ([]*models.Media, error) {
@@ -14,29 +15,5 @@ func (r *queryResolver) MyTimeline(ctx context.Context, paginate *models.Paginat
 		return nil, auth.ErrUnauthorized
 	}
 
-	query := r.Database.
-		Joins("JOIN albums ON media.album_id = albums.id").
-		Where("albums.id IN (?)", r.Database.Table("user_albums").Select("user_albums.album_id").Where("user_id = ?", user.ID)).
-		Order("YEAR(media.date_shot) DESC").
-		Order("MONTH(media.date_shot) DESC").
-		Order("DAY(media.date_shot) DESC").
-		Order("albums.title ASC").
-		Order("TIME(media.date_shot) DESC")
-
-	if fromDate != nil {
-		query = query.Where("media.date_shot < ?", fromDate)
-	}
-
-	if onlyFavorites != nil && *onlyFavorites == true {
-		query = query.Where("media.id IN (?)", r.Database.Table("user_media_data").Select("user_media_data.media_id").Where("user_media_data.user_id = ?", user.ID).Where("user_media_data.favorite = 1"))
-	}
-
-	query = models.FormatSQL(query, nil, paginate)
-
-	var media []*models.Media
-	if err := query.Find(&media).Error; err != nil {
-		return nil, err
-	}
-
-	return media, nil
+	return actions.MyTimeline(r.Database, user, paginate, onlyFavorites, fromDate)
 }
