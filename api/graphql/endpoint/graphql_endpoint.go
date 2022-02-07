5ffc1a57dd33f5efab7670c29456bdaef409406b
viktorstrate <viktorstrate@gmail.com>
2022-02-07 18:15:47 +0100

    Reconnect custom websocket upgrader

--- api/graphql/endpoint/graphql_endpoint.go
+++ api/graphql/endpoint/graphql_endpoint.go
@@ -9,6 +9,7 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler/transport"
 	photoview_graphql "github.com/photoview/photoview/api/graphql"
 	"github.com/photoview/photoview/api/graphql/resolvers"
+	"github.com/photoview/photoview/api/server"
 	"github.com/photoview/photoview/api/utils"
 	"gorm.io/gorm"
 )
@@ -27,6 +28,7 @@ func GraphqlEndpoint(db *gorm.DB) *graphql_handler.Server {
 	graphqlServer := graphql_handler.New(photoview_graphql.NewExecutableSchema(graphqlConfig))
 	graphqlServer.AddTransport(transport.Websocket{
 		KeepAlivePingInterval: 10 * time.Second,
+		Upgrader:              server.WebsocketUpgrader(utils.DevelopmentMode()),
 	})
 	graphqlServer.AddTransport(transport.Options{})
 	graphqlServer.AddTransport(transport.GET{})
