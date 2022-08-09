ca889d02bd81f99e4703d0042ca58c7d1a5f2c16
PJ-Watson <59647390+PJ-Watson@users.noreply.github.com>
2022-08-09 06:43:25 +0100

    Fixed site_info_test - missing model import.

--- api/graphql/models/site_info_test.go
+++ api/graphql/models/site_info_test.go
@@ -22,7 +22,7 @@ func TestSiteInfo(t *testing.T) {
 	site_info.InitialSetup = false
 	site_info.PeriodicScanInterval = 360
 	site_info.ConcurrentWorkers = 10
-	site_info.ThumbnailMethod = ThumbnailFilterLanczos
+	site_info.ThumbnailMethod = models.ThumbnailFilterLanczos
 
 	if !assert.NoError(t, db.Session(&gorm.Session{AllowGlobalUpdate: true}).Save(&site_info).Error) {
 		return
@@ -37,7 +37,7 @@ func TestSiteInfo(t *testing.T) {
 		InitialSetup:         false,
 		PeriodicScanInterval: 360,
 		ConcurrentWorkers:    10,
-		ThumbnailMethod:    	ThumbnailFilterLanczos,
+		ThumbnailMethod:    	models.ThumbnailFilterLanczos,
 	}, *site_info)
 
 }
