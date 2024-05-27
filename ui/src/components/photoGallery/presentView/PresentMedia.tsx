c9115495a704ca5a05eee87db12c41987fd0c5c7
printfuck <stuff.github@eris.cc>
2024-05-27 16:03:55 +0200

    minor fix

--- ui/src/components/photoGallery/presentView/PresentMedia.tsx
+++ ui/src/components/photoGallery/presentView/PresentMedia.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import { MediaType } from '../../../__generated__/globalTypes'
 import { exhaustiveCheck } from '../../../helpers/utils'
