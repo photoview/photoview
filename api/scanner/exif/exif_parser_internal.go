44565a90bb1e8a82a21d41075c6a63839963b708
Konstantin Koval <kkb@ukr.net>
2024-05-28 17:32:46 +0300

    corrected the syntax

--- api/scanner/exif/exif_parser_internal.go
+++ api/scanner/exif/exif_parser_internal.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"math"
+	"math/big"
 	"os"
 	"time"
 
@@ -173,7 +174,7 @@ func (p *internalExifParser) readStringTag(tags *exif.Exif, name exif.FieldName,
 	return nil, errors.New("exif tag returned null")
 }
 
-func (p *internalExifParser) readRationalTag(tags *exif.Exif, name exif.FieldName, media_path string) (*math.big.Rat, error) {
+func (p *internalExifParser) readRationalTag(tags *exif.Exif, name exif.FieldName, media_path string) (*big.Rat, error) {
 	tag, err := tags.Get(name)
 	if err != nil {
 		return nil, errors.Wrapf(err, "could not read %s from EXIF: %s", name, media_path)
