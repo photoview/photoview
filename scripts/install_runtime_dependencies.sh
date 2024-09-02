12a9f76275b3272cd350377584abe17923ef720e
Googol Lee <i@googol.im>
2024-09-02 10:05:14 +0200

    Add extra libraries for imagemagick and ffmpeg.

--- scripts/install_runtime_dependencies.sh
+++ scripts/install_runtime_dependencies.sh
@@ -3,7 +3,9 @@ set -euo pipefail
 
 apk update
 
-apk add curl libheif lapack cblas ffmpeg exiftool imagemagick imagemagick-heic imagemagick-jpeg imagemagick-raw imagemagick-tiff
+apk add curl libheif lapack cblas exiftool
+apk add ffmpeg ffmpeg-libs ffmpeg-libavcodec ffmpeg-libavformat
+apk add imagemagick imagemagick-libs imagemagick-heic imagemagick-jpeg imagemagick-raw imagemagick-tiff imagemagick-webp imagemagick-svg imagemagick-jxl
 apk add dlib --repository=https://dl-cdn.alpinelinux.org/alpine/edge/testing
 
 # Remove build dependencies and cleanup
