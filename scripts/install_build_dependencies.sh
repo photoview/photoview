85c1e52b061ea2b4ee24ecca9113922230ca460a
Googol Lee <i@googol.im>
2024-09-02 18:35:45 +0200

    Use alpine as base images. (#1033)

--- scripts/install_build_dependencies.sh
+++ scripts/install_build_dependencies.sh
@@ -1,42 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 set -euo pipefail
 
-if [ "$TARGETPLATFORM" == "linux/arm64" ]; then
-  dpkg --add-architecture arm64
-  DEBIAN_ARCH='arm64'
-elif [ "$TARGETPLATFORM" == "linux/arm/v6" ] || [ "$TARGETPLATFORM" == "linux/arm/v7" ]; then
-  dpkg --add-architecture armhf
-  DEBIAN_ARCH='armhf'
-else
-  dpkg --add-architecture amd64
-  DEBIAN_ARCH='amd64'
-fi
+apk update
 
-apt-get update
-
-# Install G++/GCC cross compilers
-if [ "$DEBIAN_ARCH" == "arm64" ]; then
-  apt-get install -y \
-    g++-aarch64-linux-gnu \
-    libc6-dev-arm64-cross
-elif [ "$DEBIAN_ARCH" == "armhf" ]; then
-  apt-get install -y \
-    g++-arm-linux-gnueabihf \
-    libc6-dev-armhf-cross
-else
-  apt-get install -y \
-    g++-x86-64-linux-gnu \
-    libc6-dev-amd64-cross
-fi
-
-# Install go-face dependencies and libheif for HEIF media decoding
-apt-get install -y \
-  libdlib-dev:${DEBIAN_ARCH} \
-  libblas-dev:${DEBIAN_ARCH} \
-  libatlas-base-dev:${DEBIAN_ARCH} \
-  liblapack-dev:${DEBIAN_ARCH} \
-  libjpeg-dev:${DEBIAN_ARCH} \
-  libheif-dev:${DEBIAN_ARCH}
+apk add go g++ blas-dev cblas lapack-dev jpeg-dev libheif-dev
+apk add dlib-dev --repository=https://dl-cdn.alpinelinux.org/alpine/edge/testing
 
 # Install tools for development
-apt-get install -y reflex sqlite3
+apk add sqlite
+go install github.com/cespare/reflex@latest
--- scripts/install_runtime_dependencies.sh
+++ scripts/install_runtime_dependencies.sh
@@ -1,10 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 set -euo pipefail
 
-apt-get update
-apt-get install -y curl libdlib19.2 ffmpeg exiftool libheif1 imagemagick
+apk update
 
-# Remove build dependencies and cleanup
-apt-get autoremove -y
-apt-get clean
-rm -rf /var/lib/apt/lists/*
+apk add curl libheif lapack cblas exiftool
+apk add ffmpeg ffmpeg-libs ffmpeg-libavcodec ffmpeg-libavformat
+apk add imagemagick imagemagick-libs imagemagick-heic imagemagick-jpeg imagemagick-raw imagemagick-tiff imagemagick-webp imagemagick-svg imagemagick-jxl
+apk add dlib --repository=https://dl-cdn.alpinelinux.org/alpine/edge/testing
