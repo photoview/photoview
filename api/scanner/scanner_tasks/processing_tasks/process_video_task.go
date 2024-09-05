bee0787ed4ce5c3bc61c308041608a0f09ef1f54
bzzim <bzz445@gmail.com>
2024-09-05 15:01:30 +0500

    Fix wrap nil error
    In this case ReadVideoStreamMetadata return nil, nil because: 
    
    func Wrap(err error, message string) error {
    	if err == nil {
    		return nil
    	}
    	.....
    }

--- api/scanner/scanner_tasks/processing_tasks/process_video_task.go
+++ api/scanner/scanner_tasks/processing_tasks/process_video_task.go
@@ -225,7 +225,7 @@ func ReadVideoStreamMetadata(videoPath string) (*ffprobe.Stream, error) {
 
 	stream := data.FirstVideoStream()
 	if stream == nil {
-		return nil, errors.Wrapf(err, "could not get stream from file metadata (%s)", path.Base(videoPath))
+		return nil, errors.New(fmt.Sprintf("could not get stream from file metadata (%s)", path.Base(videoPath)))
 	}
 
 	return stream, nil
