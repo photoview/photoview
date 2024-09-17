28a7f0ac7515144f8c8d34b5b45255bd8f55b272
Googol Lee <i@googol.im>
2024-09-17 10:33:37 +0200

    Update with comments.

--- scripts/install_runtime_dependencies.sh
+++ scripts/install_runtime_dependencies.sh
@@ -2,12 +2,15 @@
 set -eu
 
 apt-get update
-apt-get install -y curl exiftool
 
-# libheif dependencies: no dependency
-# libraw dependencies
-apt-get install -y libjpeg62-turbo liblcms2-2
-# ImageMagick dependencies
-apt-get install -y libjbig0 libtiff6 libfreetype6 libjxl0.7 liblqr-1-0 libpng16-16 libdjvulibre21 libwebpmux3 libwebpdemux2 libwebp7 libopenexr-3-1-30 libopenjp2-7 libjpeg62-turbo liblcms2-2 libxml2 libx11-6 libgomp1
-# go-face dependencies
-apt-get install -y libdlib19.1 libblas3 liblapack3 libjpeg62-turbo
+apt-get install -y \
+  curl exiftool \
+  # libheif dependencies: no dependency
+  # libraw dependencies
+  libjpeg62-turbo liblcms2-2 \
+  # ImageMagick dependencies
+  libjbig0 libtiff6 libfreetype6 libjxl0.7 liblqr-1-0 libpng16-16 \
+  libdjvulibre21 libwebpmux3 libwebpdemux2 libwebp7 libopenexr-3-1-30 \
+  libopenjp2-7 libjpeg62-turbo liblcms2-2 libxml2 libx11-6 libgomp1 \
+  # go-face dependencies
+  libdlib19.1 libblas3 liblapack3 libjpeg62-turbo
