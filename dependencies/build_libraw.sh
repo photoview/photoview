9547be075c9217dfe57fd26652ccd53a220a01f4
Googol Lee <i@googol.im>
2024-09-30 17:16:09 +0200

    Build libheif with all dependencies in one so file.

--- dependencies/build_libraw.sh
+++ dependencies/build_libraw.sh
@@ -6,7 +6,7 @@ set -eu
 
 echo Compiler: ${DEB_HOST_MULTIARCH} Arch: ${DEB_HOST_ARCH}
 
-apt-get install -y zlib1g-dev:${DEB_HOST_ARCH} liblcms2-dev:${DEB_HOST_ARCH} libjpeg-dev:${DEB_HOST_ARCH}
+apt-get install -y libjpeg62-turbo-dev:${DEB_HOST_ARCH} liblcms2-dev:${DEB_HOST_ARCH} zlib1g-dev:${DEB_HOST_ARCH}
 URL=$(curl -s https://api.github.com/repos/LibRaw/LibRaw/releases/latest | grep "tarball_url" | cut -d : -f 2,3 | tr -d ' ,"')
 echo download libraw from $URL
 curl -L -o ./libraw.tar.gz "$URL"
@@ -19,7 +19,8 @@ make
 make install
 cd ..
 
-mkdir -p /output/lib /output/include /output/pkgconfig
+mkdir -p /output/bin /output/lib /output/include /output/pkgconfig
+cp -a /usr/local/bin/raw* /output/bin/
 cp -a /usr/local/lib/libraw_r* /output/lib/
 cp -a /usr/local/lib/pkgconfig/libraw* /output/pkgconfig/
 cp -a /usr/local/include/libraw /output/include/
