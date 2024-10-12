4fc9edbd782b216de9cc1193a218fced328fb6ee
Googol Lee <i@googol.im>
2024-10-12 16:38:38 +0200

    Update dep (#1091)
    * Update with new deps.
    
    * Update with libheif, removing encoders.

--- scripts/install_build_dependencies.sh
+++ scripts/install_build_dependencies.sh
@@ -5,11 +5,11 @@ set -eu
 echo Arch: ${DEB_HOST_ARCH}
 
 # Install go-heif dependencies
-apt-get install -y libaom-dev:${DEB_HOST_ARCH} libdav1d-dev:${DEB_HOST_ARCH} libde265-dev:${DEB_HOST_ARCH} libjpeg62-turbo-dev:${DEB_HOST_ARCH} libnuma-dev:${DEB_HOST_ARCH} libpng-dev:${DEB_HOST_ARCH} libx265-dev:${DEB_HOST_ARCH} zlib1g-dev:${DEB_HOST_ARCH}
+apt-get install -y libdav1d-dev:${DEB_HOST_ARCH} libde265-dev:${DEB_HOST_ARCH} libjpeg62-turbo-dev:${DEB_HOST_ARCH} libpng-dev:${DEB_HOST_ARCH} libnuma-dev:${DEB_HOST_ARCH} zlib1g-dev:${DEB_HOST_ARCH}
 
 # Install go-face dependencies
 apt-get install -y \
-  libdlib-dev:${DEB_HOST_ARCH} libblas-dev:${DEB_HOST_ARCH} libatlas-base-dev:${DEB_HOST_ARCH} liblapack-dev:${DEB_HOST_ARCH} libjpeg-dev:${DEB_HOST_ARCH}
+  libdlib-dev:${DEB_HOST_ARCH} libblas-dev:${DEB_HOST_ARCH} libatlas-base-dev:${DEB_HOST_ARCH} liblapack-dev:${DEB_HOST_ARCH} libjpeg62-turbo-dev:${DEB_HOST_ARCH}
 
 # Install tools for development
 apt-get install -y reflex sqlite3
