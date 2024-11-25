6c020cc1f9d74696f14ee1fa42234ea44ba7afc5
Kostiantyn <32730812+kkovaletp@users.noreply.github.com>
2024-11-25 16:25:38 +0200

    Enhance Codecov integration (#1136)
    * Define sub-projects and set coverage threshold for failure in the `codecov.yml`; make both test execution engines to generate also JUnit reports; upload these JUnit reports to Codecov for analysis
    
    * Fix UI Vite config and add more details to the API is-generated-code script
    
    * Add API JUnit report file to `.gitignore`
    
    * Fix the copy issue by mounting the volume
    
    * put the exact report name to the `docker cp` command
    
    * Added `set -euxo pipeline` to the `test_api_coverage.sh` script; try to fix the missing report issue by setting immutable flag and report directoty
    
    * Revert `chattr` and `set` commands, as they cause errors
    
    * debug
    
    * put the filename of the JUnit report to upload
    
    * `set -euxo pipefail` in `scripts/test_api_coverage.sh`
    
    ---------
    
    Co-authored-by: Konstantin Koval

--- scripts/test_is_generated_code_in_sync.sh
+++ scripts/test_is_generated_code_in_sync.sh
@@ -5,6 +5,8 @@ cd $(dirname $0)/../api
 go generate ./...
 if [ "$(git status -s 2>/dev/null | head -1)" != "" ]; then
   echo '--- FAIL: The generated API code is out of sync with the recent changes. Please run `go generate ./...` under `./api` to regenerate it and commit it to this branch.'
+  echo 'These are the changes:'
+  git status -s
   exit 1
 fi
 
