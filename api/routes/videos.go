564c41e153e74db677b7324360024b88fe3ea901
Konstantin Koval <kkb@ukr.net>
2025-05-09 21:32:19 +0300

    Refactored code; temp disable workflows on push

--- api/routes/videos.go
+++ api/routes/videos.go
@@ -1,14 +1,15 @@
 package routes
 
 import (
-	"log"
 	"net/http"
 	"os"
 	"path"
 	"strconv"
+	"strings"
 
 	"github.com/gorilla/mux"
 	"github.com/photoview/photoview/api/graphql/models"
+	"github.com/photoview/photoview/api/log"
 	"github.com/photoview/photoview/api/scanner"
 	"github.com/photoview/photoview/api/utils"
 	"gorm.io/gorm"
@@ -19,19 +20,31 @@ func RegisterVideoRoutes(db *gorm.DB, router *mux.Router) {
 	router.HandleFunc("/{name}", func(w http.ResponseWriter, r *http.Request) {
 		mediaName := mux.Vars(r)["name"]
 
-		var mediaURL models.MediaURL
-		result := db.Model(&models.MediaURL{}).Select("media_urls.*").Joins("Media").Where("media_urls.media_name = ?", mediaName).Find(&mediaURL)
-		if err := result.Error; err != nil {
+		var mediaURLs []models.MediaURL
+		if err := db.Model(&models.MediaURL{}).
+			Select("media_urls.*").
+			Joins("Media").
+			Where("media_urls.media_name = ? AND media_urls.purpose = ?", mediaName, models.VideoWeb).
+			Find(&mediaURLs).
+			Error; err != nil || len(mediaURLs) == 0 || mediaURLs[0].Media == nil {
+
 			w.WriteHeader(http.StatusNotFound)
 			w.Write([]byte("404"))
 			return
 		}
 
+		if len(mediaURLs) > 1 {
+			sanitizedMediaName := strings.ReplaceAll(mediaName, "\n", "")
+			sanitizedMediaName = strings.ReplaceAll(sanitizedMediaName, "\r", "")
+			log.Warn("Multiple video web URLs found for name %s: %d", sanitizedMediaName, len(mediaURLs))
+		}
+
+		mediaURL := mediaURLs[0]
 		var media = mediaURL.Media
 
 		if success, response, status, err := authenticateMedia(media, db, r); !success {
 			if err != nil {
-				log.Printf("WARN: error authenticating video: %s\n", err)
+				log.Warn("got error authenticating video: %s", err)
 			}
 			w.WriteHeader(status)
 			w.Write([]byte(response))
@@ -43,7 +56,7 @@ func RegisterVideoRoutes(db *gorm.DB, router *mux.Router) {
 		if mediaURL.Purpose == models.VideoWeb {
 			cachedPath = path.Join(utils.MediaCachePath(), strconv.Itoa(int(media.AlbumID)), strconv.Itoa(int(mediaURL.MediaID)), mediaURL.MediaName)
 		} else {
-			log.Printf("ERROR: Can not handle media_purpose for video: %s\n", mediaURL.Purpose)
+			log.Error("Can not handle media_purpose for video: %s", mediaURL.Purpose)
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte(internalServerError))
 			return
@@ -52,14 +65,14 @@ func RegisterVideoRoutes(db *gorm.DB, router *mux.Router) {
 		if _, err := os.Stat(cachedPath); err != nil {
 			if os.IsNotExist(err) {
 				if err := scanner.ProcessSingleMedia(db, media); err != nil {
-					log.Printf("ERROR: processing video not found in cache: %s\n", err)
+					log.Error("processing video not found in cache: %s", err)
 					w.WriteHeader(http.StatusInternalServerError)
 					w.Write([]byte(internalServerError))
 					return
 				}
 
 				if _, err := os.Stat(cachedPath); err != nil {
-					log.Printf("ERROR: after reprocessing video not found in cache: %s\n", err)
+					log.Error("video not found in cache after reprocessing: %s", err)
 					w.WriteHeader(http.StatusInternalServerError)
 					w.Write([]byte(internalServerError))
 					return
