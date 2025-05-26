9ca6b6ffffe59ce2a36c4048b69d274e46a4b9b6
Googol Lee <i@googol.im>
2025-05-26 13:31:00 +0200

    Add building dependencies.

--- scripts/install_runtime_dependencies.sh
+++ scripts/install_runtime_dependencies.sh
@@ -6,11 +6,15 @@ apt-get install -y curl exiftool
 
 # libheif dependencies
 apt-get install -y libdav1d6 librav1e0 libde265-0 libx265-199 libjpeg62-turbo libopenh264-7 libpng16-16 libnuma1 zlib1g
+
 # libraw dependencies
 apt-get install -y libjpeg62-turbo liblcms2-2 zlib1g libgomp1
+
 # ImageMagick dependencies
 apt-get install -y libjxl0.7 liblcms2-2 liblqr-1-0 libdjvulibre21 libjpeg62-turbo libopenjp2-7 libopenexr-3-1-30 libpng16-16 libtiff6 libwebpmux3 libwebpdemux2 libwebp7 libxml2 zlib1g liblzma5 libbz2-1.0 libgomp1
+
 # go-face dependencies
 apt-get install -y libdlib19.1 libblas3 liblapack3 libjpeg62-turbo
-# libmagic dependencies
+
+# gomagic dependencies
 apt-get install -y libmagic1
