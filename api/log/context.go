d4b120bfe469e410d68592a97d0c6f818ca5807e
Googol Lee <i@googol.im>
2025-06-06 15:50:10 +0200

    Update log for passing attrs with context.

---
+++ api/log/context.go
@@ -0,0 +1,35 @@
+package log
+
+import (
+	"context"
+	"log/slog"
+)
+
+var defaultLogger *slog.Logger
+
+func init() {
+	defaultLogger = slog.Default()
+}
+
+type loggerKeyType string
+
+const loggerKey loggerKeyType = "logger"
+
+func getLogger(ctx context.Context) *slog.Logger {
+	if ctx == nil {
+		return defaultLogger
+	}
+
+	logger := ctx.Value(loggerKey)
+	if logger == nil {
+		return defaultLogger
+	}
+
+	return logger.(*slog.Logger)
+}
+
+func WithAttrs(ctx context.Context, args ...any) context.Context {
+	old := getLogger(ctx)
+	new := old.With(args...)
+	return context.WithValue(ctx, loggerKey, new)
+}
