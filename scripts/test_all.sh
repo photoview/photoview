fa0ad8e6182cae2f1c44b4b9bfe392108f9ce4f8
Googol Lee <i@googol.im>
2025-07-05 09:17:56 +0200

    Use golang coverage for codecov.

--- scripts/test_all.sh
+++ scripts/test_all.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -eu
 
-go install github.com/jstemmer/go-junit-report@latest
-
 for test in $(dirname $0)/test_*; do
   if [ "${test}" != "${test%%/scripts/test_all.sh}" ]; then
     continue
--- scripts/test_api_coverage.sh
+++ scripts/test_api_coverage.sh
@@ -2,4 +2,4 @@
 set -euxo pipefail
 
 cd "$(dirname $0)/../api"
-go test ./... -v -database -filesystem -p 1 -race -coverpkg=./... -coverprofile=coverage.txt -covermode=atomic 2>&1 | tee >(go-junit-report >test-api-coverage-report.xml)
+go test ./... -v -database -filesystem -p 1 -race -coverpkg=./... -coverprofile=test-api-coverage-report.txt -covermode=atomic
