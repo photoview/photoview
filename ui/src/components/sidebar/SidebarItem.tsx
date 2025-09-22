66d1221a7ce5894a4b8e03a208fb545527f726f9
Konstantin Koval <kkb@ukr.net>
2025-09-22 17:44:04 +0300

    Enhance the layout

--- ui/src/components/sidebar/SidebarItem.tsx
+++ ui/src/components/sidebar/SidebarItem.tsx
@@ -6,11 +6,11 @@ type SidebarItemProps = {
 }
 
 const SidebarItem = ({ name, value }: SidebarItemProps) => (
-  <div className="grid grid-cols-[100px_1fr] gap-2 items-start">
-    <div className="font-semibold text-sm text-[#888] text-right">
+  <div className="grid grid-cols-[8rem_1fr] md:grid-cols-[12rem_1fr] gap-2 items-start">
+    <div className="font-semibold text-sm text-gray-500 dark:text-gray-400 text-right">
       {name}
     </div>
-    <div className="text-base break-words">
+    <div className="text-base break-words select-text">
       {value}
     </div>
   </div>
