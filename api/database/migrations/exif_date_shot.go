b4c70c4eb0909ecd35f60e229d65c7a8f5c96e82
Googol Lee <i@googol.im>
2025-10-06 14:05:31 +0200

    Fix migrate code.

--- api/database/migrations/exif_date_shot.go
+++ api/database/migrations/exif_date_shot.go
@@ -10,18 +10,19 @@ import (
 
 func MigrateDateShot(db *gorm.DB) error {
 	return db.Transaction(func(tx *gorm.DB) error {
-		rows, err := tx.Model(&models.MediaEXIF{}).Select("date_shot").Where("date_shot IS NOT NULL AND date_shot_str IS NULL").Rows()
+		rows, err := tx.Model(&models.MediaEXIF{}).Select("id", "date_shot").Where("date_shot IS NOT NULL AND date_shot_str IS NULL").Rows()
 		if err != nil {
 			return fmt.Errorf("can't query rows for migrating date_shot: %w", err)
 		}
 
 		for rows.Next() {
+			var id int
 			var dateShot time.Time
-			if err := rows.Scan(&dateShot); err != nil {
+			if err := rows.Scan(&id, &dateShot); err != nil {
 				return fmt.Errorf("can't query rows for date_shot: %w", err)
 			}
 
-			if err := tx.Model(&models.MediaEXIF{}).Update("date_shot_str", dateShot.Format(models.RFC3339MilliWithoutTimezone)).Error; err != nil {
+			if err := tx.Model(&models.MediaEXIF{}).Where("id = ?", id).Update("date_shot_str", dateShot.Format(models.RFC3339MilliWithoutTimezone)).Error; err != nil {
 				return fmt.Errorf("can't update rows for date_shot_at: %w", err)
 			}
 		}
