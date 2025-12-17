32b1c1ba2827d3e67ae8bdc46ed114f35bcb5e7c
~eMAD~ <emad.alam@yahoo.com>
2025-12-18 00:22:45 +0100

    fix cookie operations for authentication

--- ui/src/helpers/authentication.ts
+++ ui/src/helpers/authentication.ts
@@ -1,25 +1,47 @@
-export function saveTokenCookie(token: string) {
-  const maxAge = 14 * 24 * 60 * 60
+import Cookies from 'js-cookie'
 
-  document.cookie = `auth-token=${token} ;max-age=${maxAge} ;path=/ ;sameSite=Lax`
+const COOKIE_DEFAULT_OPTIONS = {
+  path: '/',
+  sameSite: 'Lax',
+} as Partial<Cookies.CookieAttributes>
+
+const AUTH_TOKEN_MAX_AGE_IN_DAYS = 14
+const AUTH_TOKEN_COOKIE_NAME = 'auth-token'
+
+const SHARE_TOKEN_COOKIE_NAME = (shareToken: string) =>
+  `share-token-pw-${shareToken}`
+
+export function saveTokenCookie(token: string) {
+  const options = {
+    ...COOKIE_DEFAULT_OPTIONS,
+    expires: AUTH_TOKEN_MAX_AGE_IN_DAYS,
+  }
+
+  Cookies.set(AUTH_TOKEN_COOKIE_NAME, token, options)
 }
 
 export function clearTokenCookie() {
-  document.cookie = 'auth-token= ;max-age=0 ;path=/ ;sameSite=Lax'
+  Cookies.remove(AUTH_TOKEN_COOKIE_NAME)
 }
 
 export function authToken() {
-  const match = document.cookie.match(/auth-token=([\d\w]+)/)
-  return match && match[1]
+  return Cookies.get(AUTH_TOKEN_COOKIE_NAME)
 }
 
 export function saveSharePassword(shareToken: string, password: string) {
-  document.cookie = `share-token-pw-${shareToken}=${password} ;path=/ ;sameSite=Lax`
+  const cookieName = SHARE_TOKEN_COOKIE_NAME(shareToken)
+
+  Cookies.set(cookieName, password, COOKIE_DEFAULT_OPTIONS)
+}
+
+export function clearSharePassword(shareToken: string) {
+  const cookieName = SHARE_TOKEN_COOKIE_NAME(shareToken)
+
+  Cookies.remove(cookieName)
 }
 
 export function getSharePassword(shareToken: string) {
-  const match = document.cookie.match(
-    `share-token-pw-${shareToken}=([\\d\\w]+)`
-  )
-  return match && match[1]
+  const cookieName = SHARE_TOKEN_COOKIE_NAME(shareToken)
+
+  return Cookies.get(cookieName)
 }
