96588b3edaef528aa13fc1a5d6ec84b013cf1721
Googol Lee <i@googol.im>
2026-03-29 19:07:30 +0200

    Fix dep build scripts.

--- dependencies/build_darktable.sh
+++ dependencies/build_darktable.sh
@@ -37,13 +37,9 @@ apt-get install -y \
   iso-codes \
   xsltproc \
   libavif-dev:"${DEB_HOST_ARCH}" \
-  libcairo2-dev:"${DEB_HOST_ARCH}" \
-  libcolord-dev:"${DEB_HOST_ARCH}" \
-  libcolord-gtk-dev:"${DEB_HOST_ARCH}" \
-  libcurl4-gnutls-dev:"${DEB_HOST_ARCH}" \
+  libcurl4-openssl-dev:"${DEB_HOST_ARCH}" \
   libexiv2-dev:"${DEB_HOST_ARCH}" \
   libgmic-dev:"${DEB_HOST_ARCH}" \
-  libgphoto2-dev:"${DEB_HOST_ARCH}" \
   libgraphicsmagick1-dev:"${DEB_HOST_ARCH}" \
   libgtk-3-dev:"${DEB_HOST_ARCH}" \
   libjpeg-dev:"${DEB_HOST_ARCH}" \
@@ -56,63 +52,41 @@ apt-get install -y \
   libpng-dev:"${DEB_HOST_ARCH}" \
   libpugixml-dev:"${DEB_HOST_ARCH}" \
   librsvg2-dev:"${DEB_HOST_ARCH}" \
-  libgtk-3-dev:"${DEB_HOST_ARCH}" \
   libsqlite3-dev:"${DEB_HOST_ARCH}" \
   libtiff-dev:"${DEB_HOST_ARCH}" \
   libwebp-dev:"${DEB_HOST_ARCH}"
 
 URL="https://github.com/darktable-org/darktable.git"
 echo download Darktable repo from "$URL"
-git clone $URL darktable || true
+git clone --depth 1 --branch ${DARKTABLE_VERSION} --recurse-submodules --shallow-submodules ${URL} darktable || true
 cd darktable
-git checkout ${DARKTABLE_VERSION}
-git submodule init
-git submodule update
 
-./build.sh \
-  --prefix "/opt/darktable" \
-  --build-type "Release" \
-  --install \
+FEATURES=" \
   --disable-kwallet \
   --disable-libsecret \
   --disable-lua \
   --disable-mac_integration \
   --disable-map \
-  --disable-unity
+  --disable-unity \
+  --disable-camera \
+  --disable-colord \
+  --enable-graphicsmagick \
+  --enable-imagemagick \
+  --enable-jxl \
+  --enable-opencl \
+  --enable-openexr \
+  --enable-openmp \
+  --enable-webp"
 
-exit 0
-
-FEATURES="--with-heic --with-jpeg --with-png --with-raw --with-tiff --with-webp"
-
-./configure \
-  --enable-64bit-channel-masks \
-  --enable-static --enable-shared --enable-delegate-build \
-  --without-x --without-magick-plus-plus \
-  --without-perl --disable-doc \
-  --host="${DEB_HOST_GNU_TYPE}" \
+./build.sh \
+  --prefix "/opt/darktable" \
+  --build-type "Release" \
+  --install \
   ${FEATURES}
 
-# Ensure that features are enabled
-for feature in ${FEATURES}
-do
-  grep -- ${feature}'.*yes$' config.log || (echo "Can't enable feature ${feature}"; false)
-done
-
-make
-make install
-cd ..
-
-mkdir -p /output/bin /output/etc /output/lib /output/include /output/pkgconfig
-cp -a /usr/local/bin/magick /output/bin/
-cp -a /usr/local/etc/Darktable-7 /output/etc/
-cp -a /usr/local/lib/Darktable-* /output/lib/
-cp -a /usr/local/lib/libMagickCore-* /output/lib/
-cp -a /usr/local/lib/libMagickWand-* /output/lib/
-cp -a /usr/local/include/Darktable-7 /output/include/
-cp -a /usr/local/lib/pkgconfig/Darktable*.pc /output/pkgconfig/
-cp -a /usr/local/lib/pkgconfig/MagickCore*.pc /output/pkgconfig/
-cp -a /usr/local/lib/pkgconfig/MagickWand*.pc /output/pkgconfig/
-file /output/bin/magick
+mkdir -p /output/opt
+cp -a /opt /output/
+file /output/opt/darktable/bin/darktable-cli
 
 # After successful build, cache the results
 echo "Caching Darktable ${DARKTABLE_VERSION} build results..."
