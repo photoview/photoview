103960a2c353c6827f8be0352d32e92c10829eec
Googol Lee <i@googol.im>
2026-04-20 10:46:31 +0200

    Clean up old dependencies with the new image.

--- scripts/install_build_dependencies.sh
+++ scripts/install_build_dependencies.sh
@@ -41,15 +41,6 @@ LIBS=(
   # tools for development
   reflex
   sqlite3
-
-  # libheif dependencies
-  "libdav1d-dev:${DEB_HOST_ARCH}"
-  "libde265-dev:${DEB_HOST_ARCH}"
-  "libjpeg62-turbo-dev:${DEB_HOST_ARCH}"
-  "libopenh264-dev:${DEB_HOST_ARCH}"
-  "libpng-dev:${DEB_HOST_ARCH}"
-  "libnuma-dev:${DEB_HOST_ARCH}"
-  "zlib1g-dev:${DEB_HOST_ARCH}"
 )
 
 apt-get update
